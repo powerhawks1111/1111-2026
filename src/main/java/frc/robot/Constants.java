a3dbda91acc7c0b528af1c3d79272f97df4c3712
Jack McAllister <jpmca103@gmail.com>
2026-02-10 20:07:09 -0500

    rough estimate for manual autoalign pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,9 +15,9 @@ public class Constants {
             public static final int BLTurn = 8;
             public static final double sideLength = .552; //meters from one module center to the other. //.622 previously, one encoder center to another
             public static final double halfSideLength = sideLength/2;
-            public static final double kMaxVelocity = 5; //note that this value must be lower than the max speed for a swerve module, because for a given path a module may need to move further than the drivetrain.
-            public static final double kMaxAccel = 20;
-            public static final double kMaxChassisRotsPerSecond = 3 * (2 * Math.PI); //front coefficient is how many rots/sec we have. 
+            public static final double kMaxVelocity = 3.5; //note that this value must be lower than the max speed for a swerve module, because for a given path a module may need to move further than the drivetrain.
+            public static final double kMaxAccel = 5;
+            public static final double kMaxChassisRotsPerSecond = 2 * (2 * Math.PI); //front coefficient is how many rots/sec we have. 
             public static final double kMaxChassisRotsPerSecondPerSecond = 8 * (2 * Math.PI); //Acceleration
     }
     public class ModuleConst {
@@ -34,8 +34,8 @@ public class Constants {
             public static final double rotationsToMetersScaler = (kWheelCircumference/drivingWheelGearRatio);
             public static final double rpmToVelocityScaler = (kWheelCircumference)/(60*drivingWheelGearRatio); //SDS Mk4I standard gear ratio from motor to wheel, divide by 60 to go from secs to mins
 
-            public static final double kMaxModuleSpeed = 5; // 5.88 meters per second or 19.3 ft/s (max speed of SDS Mk4i with Vortex motor)
-            public static final double kMaxModuleAccel = 20;
+            public static final double kMaxModuleSpeed = 3.5; // 5.88 meters per second or 19.3 ft/s (max speed of SDS Mk4i with Vortex motor)
+            public static final double kMaxModuleAccel = 5;
             public static final double kP = 0.05;//0.228;
             public static final double kI = 0.0; //.1
             public static final double kD = 0.01; //0.0095;      
@@ -57,36 +57,36 @@ public class Constants {
         
         //REGULAR SELF-ALIGN 
         //translation section of our pid loops for auto alignment
-        public static final double kPT = 0;
+        public static final double kPT = 4.5;
         public static final double kIT = 0;
-        public static final double kDT = 0;
+        public static final double kDT = 0.9;
         
         //rotation section of our pid loops for auto alignment.
-        public static final double kPRot = 0;
+        public static final double kPRot = .05;
         public static final double kIRot = 0;
         public static final double kDRot = 0;   
 
         
         //PP STANDS FOR PATHPLANNER
         //translation section
-        public static final double kPTPP = 0;
+        public static final double kPTPP = 1;
         public static final double kITPP = 0;
         public static final double kDTPP = 0;
         
         //rotation section of our pid loops for auto alignment.
-        public static final double kPRotPP = 0;
+        public static final double kPRotPP = 1;
         public static final double kIRotPP = 0;
         public static final double kDRotPP = 0;   
 
 
         //C STANDS FOR CHOREO
         //translation section of our pid loops for auto alignment
-        public static final double kPTC = 0;
+        public static final double kPTC = 1;
         public static final double kITC = 0;
         public static final double kDTC = 0;
         
         //rotation section of our pid loops for auto alignment.
-        public static final double kPRotC = 0;
+        public static final double kPRotC = 1;
         public static final double kIRotC = 0;
         public static final double kDRotC = 0;  
     }
