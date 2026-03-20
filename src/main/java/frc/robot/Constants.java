f8f36c1c3270495df86629eabea29a7b502da4d4
blueGate3 <162855123+blueGate3@users.noreply.github.com>
2026-03-20 14:40:36 -0400

    updated const to spreadsheet. might be wrong w/ back r back L modules.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -100,21 +100,21 @@ public class Constants {
         public static final int BLTurn = 8;
 
         //shooter
-        public static final int LeftS = 11;
-        public static final int RightS = 12;
+        public static final int LeftS = 14;
+        public static final int RightS = 15;
         public static final int Hood = 13;
-        public static final int Turret = 14;
+        public static final int Turret = 12;
 
         //kicker
-        public static final int FrontK = 17;
-        public static final int BackK = 16;
+        public static final int FrontK = 10;
+        public static final int BackK = 11;
 
         //spindexer
-        public static final int Spindexer = 15;
+        public static final int Spindexer = 9;
 
         //intake
-        public static final int Rollers = 10;
-        public static final int Flipper = 9;
+        public static final int Rollers = 16;
+        public static final int Flipper = 17;
     }
 
     public class FIELD_CONST {
